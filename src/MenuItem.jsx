ee3a73d2d7fd003f01cf4bab3153097e6ec1207f
Rupam Sutar <rupamsutar281998@gmail.com>
2023-06-17 14:12:40 +0530

    Menu Item component setup

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const MenuItem = () => {
+const MenuItem = ({img, title, price, desc}) => {
   return (
-    <div>MenuItem</div>
-  )
+    <article>
+      <img src={img} alt={title} className="img" />
+      <div className="item-info">
+        <header>
+          <h5>{title}</h5>
+          <div className="item-price">${price}</div>
+        </header>
+
+        <p className="item-text">{desc}</p>
+      </div>
+    </article>
+  );
 }
 
 export default MenuItem
